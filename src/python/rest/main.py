aea6a3be939b25afe7fe903516956b6ed1a9b79d
Pedro Oliveira <pedro.sim.oliveira@gmail.com>
2020-09-25 10:58:27 +0100

    Fixed typos in python REST API

--- src/python/rest/main.py
+++ src/python/rest/main.py
@@ -1,9 +1,11 @@
+import asyncio
+from random import randint
+
 from fastapi import FastAPI
 import logging
 import os
 import uvicorn
 from pydantic import BaseModel
-
 
 from common import generate_random_output_data
 
@@ -12,7 +14,7 @@ MAX_INFERENCE_TIME_MS = int(os.getenv('MAX_INFERENCE_TIME_MS', 50))
 
 log_level = os.getenv('LOG_LEVEL', 'INFO')
 logging.basicConfig(format='%(levelname)s:%(message)s', level=getattr(logging, log_level))
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class TextCategorizationInput(BaseModel):
@@ -36,19 +38,19 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-    log.info(f'Starting FastAPI ...')
+    logger.info(f'Starting FastAPI ...')
 
 
 @app.post("/predict", response_model=TextCategorizationOutput)
 async def predict(body: TextCategorizationInput):
-    log.debug(f'Receive the following text to categorize: {body.text}')
+    logger.debug(f'Receive the following text to categorize: {body.text}')
     output_data = generate_random_output_data(body)
 
     random_inference_time_ms = randint(0, MAX_INFERENCE_TIME_MS)
     logger.debug(f'inference_time_ms: {random_inference_time_ms}')
     await asyncio.sleep(random_inference_time_ms / 1000)
 
-    log.debug(f'Predicted category: {output_data['subcategory']}')
+    logger.debug(f"Predicted category: {output_data['subcategory']}")
     response = TextCategorizationOutput(**output_data)
     return response
 
